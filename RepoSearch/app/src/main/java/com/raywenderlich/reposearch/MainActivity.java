70d667c839d0462985f112af16089f59cc6f3c52
Ivostoysd <ivo.stoyanov@smartdrive.net>
2018-04-28 13:33:41 -0700

    volley + retrroft ok

--- RepoSearch/app/src/main/java/com/raywenderlich/reposearch/MainActivity.java
+++ RepoSearch/app/src/main/java/com/raywenderlich/reposearch/MainActivity.java
@@ -20,6 +20,9 @@
  * THE SOFTWARE.
  */
 
+
+// "ʎǝʞ" = "value"
+
 package com.raywenderlich.reposearch;
 
 import android.app.AlertDialog;
@@ -55,159 +58,165 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity implements DownloadCompleteListener {
 
-  ListFragment mListFragment;
-  ProgressDialog mProgressDialog;
+    private static final String URL = "https://api.github.com";   // for retrofit
+    private static final String URL_REP = URL + "/repositories";
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    ListFragment mListFragment;
+    ProgressDialog mProgressDialog;
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    if (findViewById(R.id.fragment_container) != null) {
-      if (savedInstanceState != null) {
-        return;
-      }
-      if (isNetworkConnected()) {
-        mProgressDialog = new ProgressDialog(this);
-        mProgressDialog.setMessage("Please wait...");
-        mProgressDialog.setCancelable(false);
-        mProgressDialog.show();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        startDownload();
-      } else {
-        new AlertDialog.Builder(this)
-            .setTitle("No Internet Connection")
-            .setMessage("It looks like your internet connection is off. Please turn it " +
-                "on and try again")
-            .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-              public void onClick(DialogInterface dialog, int which) {
-              }
-            }).setIcon(android.R.drawable.ic_dialog_alert).show();
-      }
-    }
-  }
-
-  private void showListFragment(ArrayList<Repository> repositories) {
-    mListFragment = ListFragment.newInstance(repositories);
-    getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mListFragment).
-        commit();
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    int id = item.getItemId();
-
-    return id == R.id.action_settings || super.onOptionsItemSelected(item);
-
-  }
-
-  private void startDownload() {
-    makeRetrofitCalls();
-  }
-
-  private boolean isNetworkConnected() {
-    ConnectivityManager connMgr = (ConnectivityManager)
-        getSystemService(Context.CONNECTIVITY_SERVICE); // 1
-    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo(); // 2
-    return networkInfo != null && networkInfo.isConnected(); // 3
-  }
-
-  private boolean isWifiConnected() {
-    ConnectivityManager connMgr = (ConnectivityManager)
-        getSystemService(Context.CONNECTIVITY_SERVICE);
-    NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
-    return networkInfo != null && (ConnectivityManager.TYPE_WIFI == networkInfo.getType()) && networkInfo.isConnected();
-  }
-
-  @Override
-  public void downloadComplete(ArrayList<Repository> repositories) {
-    showListFragment(repositories);
-    if (mProgressDialog != null) {
-      mProgressDialog.hide();
-    }
-  }
-
-  private void makeRequestWithOkHttp(String url) {
-    OkHttpClient client = new OkHttpClient();   // 1
-    okhttp3.Request request = new okhttp3.Request.Builder().url(url).build();  // 2
-
-    client.newCall(request).enqueue(new okhttp3.Callback() { // 3
-      @Override
-      public void onFailure(okhttp3.Call call, IOException e) {
-        e.printStackTrace();
-      }
-
-      @Override
-      public void onResponse(okhttp3.Call call, okhttp3.Response response)
-          throws IOException {
-        final String result = response.body().string();  // 4
-
-        MainActivity.this.runOnUiThread(new Runnable() {
-          @Override
-          public void run() {
-            try {
-              downloadComplete(Util.retrieveRepositoriesFromResponse(result));  // 5
-            } catch (JSONException e) {
-              e.printStackTrace();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
             }
-          }
+            if (isNetworkConnected()) {
+                mProgressDialog = new ProgressDialog(this);
+                mProgressDialog.setMessage("Please wait...");
+                mProgressDialog.setCancelable(false);
+                mProgressDialog.show();
+
+                startDownload();
+            } else {
+                new AlertDialog.Builder(this)
+                        .setTitle("No Internet Connection")
+                        .setMessage("It looks like your internet connection is off. Please turn it " +
+                                "on and try again")
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        }).setIcon(android.R.drawable.ic_dialog_alert).show();
+            }
+        }
+    }
+
+    private void showListFragment(ArrayList<Repository> repositories) {
+        mListFragment = ListFragment.newInstance(repositories);
+        getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, mListFragment).
+                commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+    }
+
+    private void startDownload() {
+        makeRequestWithVolley(URL_REP);
+        //makeRequestWithOkHttp(URL_REP);
+        //makeRetrofitCalls(URL);
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE); // 1
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo(); // 2
+        return networkInfo != null && networkInfo.isConnected(); // 3
+    }
+
+    private boolean isWifiConnected() {
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return networkInfo != null && (ConnectivityManager.TYPE_WIFI == networkInfo.getType()) && networkInfo.isConnected();
+    }
+
+    @Override
+    public void downloadComplete(ArrayList<Repository> repositories) {
+        showListFragment(repositories);
+        if (mProgressDialog != null) {
+            mProgressDialog.hide();
+        }
+    }
+
+    //////////////////////////////
+    private void makeRequestWithVolley(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+
+        StringRequest request = new StringRequest(Request.Method.GET, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            downloadComplete(Util.retrieveRepositoriesFromResponse(response));
+                        } catch (JSONException e) {
+                            Toast.makeText(MainActivity.this, e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                },
+                new com.android.volley.Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, error.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+                    }
         });
-      }
-    });
-  }
+        queue.add(request);
+    }
+    //////////////////////////////
+    private void makeRetrofitCalls(String url) {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(url) // 1
+                .addConverterFactory(GsonConverterFactory.create()) // 2
+                .build();
 
-  private void makeRequestWithVolley(String url) {
+        RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class); // 3
 
-    RequestQueue queue = Volley.newRequestQueue(this); // 1
+        Call<ArrayList<Repository>> call = retrofitAPI.retrieveRepositories(); // 4
 
-    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-        new com.android.volley.Response.Listener<String>() { // 2
-          @Override
-          public void onResponse(String response) {
-            try {
-              downloadComplete(Util.retrieveRepositoriesFromResponse(response)); // 3
-            } catch (JSONException e) {
-              e.printStackTrace();
+        call.enqueue(new Callback<ArrayList<Repository>>() {  // 5
+            @Override
+            public void onResponse(Call<ArrayList<Repository>> call,
+                                   Response<ArrayList<Repository>> response) {
+                downloadComplete(response.body());  // 6
             }
-          }
-        }, new com.android.volley.Response.ErrorListener() {
-      @Override
-      public void onErrorResponse(VolleyError error) {
-      }
-    });
-    queue.add(stringRequest);  // 4
 
-  }
+            @Override
+            public void onFailure(Call<ArrayList<Repository>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 
-  private void makeRetrofitCalls() {
-    Retrofit retrofit = new Retrofit.Builder()
-        .baseUrl("https://api.github.com") // 1
-        .addConverterFactory(GsonConverterFactory.create()) // 2
-        .build();
+    private void makeRequestWithOkHttp(String url) {
+        OkHttpClient client = new OkHttpClient();   // 1
+        okhttp3.Request request = new okhttp3.Request.Builder().url(url).build();  // 2
 
-    RetrofitAPI retrofitAPI = retrofit.create(RetrofitAPI.class); // 3
+        client.newCall(request).enqueue(new okhttp3.Callback() { // 3
+            @Override
+            public void onFailure(okhttp3.Call call, IOException e) {
+                e.printStackTrace();
+            }
 
-    Call<ArrayList<Repository>> call = retrofitAPI.retrieveRepositories(); // 4
+            @Override
+            public void onResponse(okhttp3.Call call, okhttp3.Response response)
+                    throws IOException {
+                final String result = response.body().string();  // 4
 
-    call.enqueue(new Callback<ArrayList<Repository>>() {  // 5
-      @Override
-      public void onResponse(Call<ArrayList<Repository>> call,
-                             Response<ArrayList<Repository>> response) {
-        downloadComplete(response.body());  // 6
-      }
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            downloadComplete(Util.retrieveRepositoriesFromResponse(result));  // 5
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+            }
+        });
+    }
 
-      @Override
-      public void onFailure(Call<ArrayList<Repository>> call, Throwable t) {
-        Toast.makeText(MainActivity.this, t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
-      }
-    });
-  }
+
 }
